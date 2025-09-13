bde3bf31e4133eb764af7ae376940db560dcdad9
cantaruttim <cantaruttim@outlook.com>
2025-09-13 10:13:23 -0300

    package model created

---
+++ model/forms.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+type Forms struct {
+	NomeAluno      string `json:"name"`
+	MatriculaAluno string `json:"registrationNumber"`
+	EmailAluno     string `json:"email"`
+	ModuloAluno    string `json:"module"`
+	QuestionOne    string `json:"questionOne"`
+	QuestionTwo    string `json:"questionTwo"`
+	SetAt          time.Time
+}
+
+func (f Forms) GetFullForms() string {
+	return fmt.Sprintf(
+		"Name: %s\nRegistrationNumber: %s\nEmail: %s\nModule: %s\nQuestion One: %s\nQuestion Two: %s\nCreated At: %s",
+		f.NomeAluno,
+		f.MatriculaAluno,
+		f.EmailAluno,
+		f.ModuloAluno,
+		f.QuestionOne,
+		f.QuestionTwo,
+		f.SetAt.Format("2006-01-02 15:04:00"),
+	)
+}
