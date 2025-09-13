aac55f6def19f51b2b394850bd1a55b13d8955e8
cantaruttim <cantaruttim@outlook.com>
2025-09-13 10:13:52 -0300

    updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
-
-type Forms struct {
-	NomeAluno      string `json:"name"`
-	MatriculaAluno string `json:"registrationNumber"`
-	EmailAluno     string `json:"email"`
-	ModuloAluno    string `json:"module"`
-	QuestionOne    string `json:"questionOne"`
-	QuestionTwo    string `json:"questionTwo"`
-	SetAt          time.Time
-}
-
-func (f Forms) GetFullForms() string {
-	return fmt.Sprintf(
-		"Name: %s\nRegistrationNumber: %s\nEmail: %s\nModule: %s\nQuestion One: %s\nQuestion Two: %s\nCreated At: %s",
-		f.NomeAluno,
-		f.MatriculaAluno,
-		f.EmailAluno,
-		f.ModuloAluno,
-		f.QuestionOne,
-		f.QuestionTwo,
-		f.SetAt.Format("2006-01-02 15:04:00"),
-	)
-}
 
 func main() {
 	server := gin.Default()
