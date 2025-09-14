2ac390655d2c3d9daf2a4f0a0317e103d7bfc22c
cantaruttim <cantaruttim@outlook.com>
2025-09-13 23:01:46 -0300

    CreatForm

--- repository/formsRepository.go
+++ repository/formsRepository.go
@@ -18,10 +18,10 @@ func NewFormsRepository(
 	}
 }
 
-func (pr *FormsRepository) GetForms() ([]model.Forms, error) {
+func (fr *FormsRepository) GetForms() ([]model.Forms, error) {
 	query := "select registrationNumber, name, email, module, questionOne, questionTwo, SentAt  from activities"
 
-	rows, err := pr.connection.Query(query)
+	rows, err := fr.connection.Query(query)
 	if err != nil {
 		fmt.Println(err)
 		return []model.Forms{}, err
@@ -50,4 +50,35 @@ func (pr *FormsRepository) GetForms() ([]model.Forms, error) {
 	}
 	rows.Close()
 	return activitiesList, nil
+}
+
+func (fr *FormsRepository) CreateForms(form model.Forms) error {
+
+	_, err := fr.connection.Exec(`
+		INSERT INTO activities (
+			registration_number,
+			name,
+			email,
+			module,
+			question_one,
+			question_two,
+			sent_at
+		) VALUES ($1, $2, $3, $4, $5, $6, $7)
+	`,
+		form.RegistrationNumber,
+		form.Name,
+		form.Email,
+		form.Module,
+		form.QuestionOne,
+		form.QuestionTwo,
+		form.SentAt,
+	)
+
+	if err != nil {
+		fmt.Println("Erro ao inserir no banco:", err)
+		return err
+	}
+
+	fmt.Println("✅ Formulário inserido com sucesso!")
+	return nil
 }
