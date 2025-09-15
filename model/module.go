18006c1f176d17688aa44fb0a804baf72ef6fe19
cantaruttim <cantaruttim@outlook.com>
2025-09-14 22:35:25 -0300

    StudentModel

---
+++ model/module.go
@@ -0,0 +1,9 @@
+package model
+
+type StudentModel struct {
+	RegistrationNumber string
+	Module             string
+	Course             string
+	DataBegan          string
+	DataEnd            string
+}
